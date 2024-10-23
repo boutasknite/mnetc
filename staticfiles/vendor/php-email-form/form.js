ea245691f6699b47b2a095fd5290be3f9e910a8f
boutasknite <129511784+boutasknite@users.noreply.github.com>
2024-10-23 23:35:49 +0100

    8

---
+++ staticfiles/vendor/php-email-form/form.js
@@ -0,0 +1,20 @@
+
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach( function(e) {
+    e.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let thisForm = this;
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+
+  }
+
+
+})();
